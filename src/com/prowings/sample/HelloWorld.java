13680f1185d11948b2c5fb4dbb02a301a1092954
JD <jayprowings@gmail.com>
2024-07-27 10:07:01 +0530

    added first draft

---
+++ src/com/prowings/sample/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.prowings.sample;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hey Java developers... welcome to Git!!!");
+		
+	}
+
+}
